f4f4a199d8bac7808ca3df0c34ad57b011e004b7
Ibrahim Hassan <137226919+Ibrahim-Hassan74@users.noreply.github.com>
2024-08-09 17:09:23 +0300

    AbstractionInterface.cs

---
+++ AbstractionInterface.cs
@@ -0,0 +1,55 @@
+global using static System.Console;
+global using static System.Math;
+global using static System.ConsoleColor;
+global using main;
+using System.ComponentModel;
+using System.Collections;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using maincs.Models;
+namespace maincs.Models
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cashier c = new Cashier(new Cash());
+            c.CheckOut(99999.99m);
+            c = new Cashier(new Debit());
+            c.CheckOut(212.21m);
+            c = new Cashier(new Mastercard());
+            c.CheckOut(928981.222m);
+            c = new Cashier(new Via());
+            c.CheckOut(3412.21m);
+        }
+    }
+    public class Cashier
+    {
+        private IPayment _payment;
+        public Cashier(IPayment payment) => _payment = payment;
+        public void CheckOut(decimal amount) => _payment.Pay(amount);
+
+    }
+    public interface IPayment
+    {
+        void Pay(decimal amount);
+    }
+    public class Cash : IPayment
+    {
+        public void Pay(decimal amount) => WriteLine($"Cash Payment: ${Round(amount, 2):N0}");
+    }
+
+    public class Debit : IPayment
+    {
+        public void Pay(decimal amount) => WriteLine($"Debit Payment: ${Round(amount, 2):N0}");
+    }
+    public class Mastercard : IPayment
+    {
+        public void Pay(decimal amount) => WriteLine($"Mastercard Payment: ${Round(amount, 2):N0}");
+    }
+    public class Via : IPayment
+    {
+        public void Pay(decimal amount) => WriteLine($"Via Payment: ${Round(amount, 2):N0}"); 
+    }
+}
