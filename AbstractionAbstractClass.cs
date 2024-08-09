5ef9064a1e12ed08aff8fc25d9c365c20dd72eb9
Ibrahim Hassan <137226919+Ibrahim-Hassan74@users.noreply.github.com>
2024-08-09 17:05:46 +0300

    AbstractionAbstractClass.cs

--- AbstractionAbstractClass.cs
+++ AbstractionAbstractClass.cs
@@ -1,10 +1,55 @@
-﻿<Project Sdk="Microsoft.NET.Sdk">
+global using static System.Console;
+global using static System.Math;
+global using static System.ConsoleColor;
+global using main;
+using System.ComponentModel;
+using System.Collections;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using maincs.Models;
+namespace maincs.Models
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cashier c = new Cashier(new Cash());
+            c.CheckOut(99999.99m);
+            c = new Cashier(new Debit());
+            c.CheckOut(212.21m);
+            c = new Cashier(new Mastercard());
+            c.CheckOut(928981.222m);
+            c = new Cashier(new Via());
+            c.CheckOut(3412.21m);
+        }
+    }
+    public class Cashier
+    {
+        private IPayment _payment;
+        public Cashier(IPayment payment) => _payment = payment;
+        public void CheckOut(decimal amount) => _payment.Pay(amount);
 
-  <PropertyGroup>
-    <OutputType>Exe</OutputType>
-    <TargetFramework>net8.0</TargetFramework>
-    <ImplicitUsings>enable</ImplicitUsings>
-    <Nullable>enable</Nullable>
-  </PropertyGroup>
+    }
+    public abstract class IPayment
+    {
+        public abstract void Pay(decimal amount);
+    }
+    public class Cash : IPayment
+    {
+        public override void Pay(decimal amount) => WriteLine($"Cash Payment: ${Round(amount, 2):N0}");
+    }
 
-</Project>
+    public class Debit : IPayment
+    {
+        public override void Pay(decimal amount) => WriteLine($"Debit Payment: ${Round(amount, 2):N0}");
+    }
+    public class Mastercard : IPayment
+    {
+        public override void Pay(decimal amount) => WriteLine($"Mastercard Payment: ${Round(amount, 2):N0}");
+    }
+    public class Via : IPayment
+    {
+        public override void Pay(decimal amount) => WriteLine($"Via Payment: ${Round(amount, 2):N0}"); 
+    }
+}
