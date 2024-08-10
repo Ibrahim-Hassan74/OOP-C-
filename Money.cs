c1001ccbb4948e115ea21f964514e3ab058116c1
Ibrahim Hassan <137226919+Ibrahim-Hassan74@users.noreply.github.com>
2024-08-10 15:34:35 +0300

    Add files via upload

---
+++ Money.cs
@@ -0,0 +1,68 @@
+﻿namespace maincs.Models
+{
+    public class Money
+    {
+        private decimal amount;
+        public decimal Amount => amount;
+        public Money(decimal amount)
+        {
+            this.amount = amount;
+        }
+        public static Money operator +(Money a, Money b)
+        {
+            return new Money(a.Amount + b.Amount);
+        }
+        public static Money operator -(Money a, Money b)
+        {
+            return new Money(a.Amount - b.Amount);
+        }
+        public static Money operator *(Money a, Money b)
+        {
+            return new Money(a.Amount * b.Amount);
+        }
+
+        public static Money operator /(Money a, Money b)
+        {
+            return new Money(a.Amount / b.Amount);
+        }
+        public static bool operator >(Money a, Money b)
+        {
+            return a.Amount > b.Amount;
+        }
+        public static bool operator <(Money a, Money b)
+        {
+            return a.Amount < b.Amount;
+        }
+
+        public static bool operator >=(Money a, Money b)
+        {
+            return a.Amount >= b.Amount;
+        }
+        public static bool operator <=(Money a, Money b)
+        {
+            return a.Amount <= b.Amount;
+        }
+
+        public static bool operator ==(Money a, Money b)
+        {
+            return a.Amount == b.Amount;
+        }
+        public static bool operator !=(Money a, Money b)
+        {
+            return a.Amount != b.Amount;
+        }
+        public static Money operator ++(Money a)
+        {
+            var val = a.Amount;
+            return new Money(++val);
+        }
+        public static Money operator --(Money a)
+        {
+            var val = a.Amount;
+            return new Money(--val);
+        }
+
+    }
+
+
+}
